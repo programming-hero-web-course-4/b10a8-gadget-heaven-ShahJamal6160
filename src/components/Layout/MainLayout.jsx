a88572311ae82511574ac95d69be2af89162cb06
ShahJamal6160 <shahj6160@gmail.com>
2024-11-03 21:40:47 +0600

    Setup react-Router, Tailwind-css, DaisyUI

---
+++ src/components/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-center">Assignment 8</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
